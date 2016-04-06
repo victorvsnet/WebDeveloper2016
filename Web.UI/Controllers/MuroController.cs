ce813baac1730f862850b7f7fdef1a3d84f97e93
victorvsnet <victorvsnet@gmail.com>
2016-04-06 16:33:50 -0500

    @Muro

---
+++ Web.UI/Controllers/MuroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.UI.Controllers
+{
+    public class MuroController : Controller
+    {
+        // GET: Muro
+        public ActionResult Muro(int iduser)
+        {
+            return View();
+        }
+    }
+}
---
+++ Web.UI/Models/MuroViewModels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.UI.Models
+{
+    /// <summary>
+    /// Detalle de la persona que cumple años
+    /// </summary>
+    public class MuroViewModels
+    {
+        public string nombre { get; set; }
+        public string ape_paterno { get; set; }
+        public string gls_estado_perfil { get; set; }
+        public string gls_Cargo { get; set; }
+        public int anexo { get; set; }
+        public int total_mensajes { get; set; }
+        public DateTime FecNacimiento { get; set; }
+        public List<Organizador> Organizadores { get; set; }
+        public List<Mensaje> Mensajes { get; set; }
+    }
+
+    /// <summary>
+    /// Lista de los organizadores del evento, cumpleaños, coordinacion
+    /// </summary>
+    public class Organizador
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ape_paterno { get; set; }
+        public string gls_Cargo { get; set; }
+    }
+
+    /// <summary>
+    /// Lista de mensajes registrado por los demas usuarios
+    /// </summary>
+    public class Mensaje
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ape_paterno { get; set; }
+        public string gls_mensaje { get; set; }
+    }
+}
